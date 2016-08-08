54a073786e56ba64f0778860619f51243817bf28
tochman <thomas@craftacademy.se>
2016-08-08 16:20:22 +0200

    adds a condition to check account balance on withdraw

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,7 +5,35 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount)
+  def withdraw(account, amount)
+    case
+    when insufficient_funds_in_account?(account, amount) then
+      return
+    else
+      perform_withdraw(account, amount)
+    end
+  end
+
+  private
+
+  def perform_withdraw(account, amount)
+    reduce_atm_funds(amount)
+    reduce_the_account_balance(account, amount)
+    { status: true,
+      message: 'success',
+      date: Date.today,
+      amount: amount }
+  end
+
+  def reduce_atm_funds(amount)
     @funds -= amount
+  end
+
+  def reduce_the_account_balance(account, amount)
+    account.balance -= amount
+  end
+
+  def insufficient_funds_in_account?(account, amount)
+    amount > account.balance
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 require './lib/atm.rb'
 
 describe Atm do
+  let(:account) { double('Account') }
+
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+
   it 'has 1000$ when created' do
     expect(subject.funds).to eq 1000
   end
@@ -8,7 +15,16 @@ describe Atm do
   it { is_expected.to respond_to :withdraw }
 
   it 'funds are reduced at successful withdraw' do
-    subject.withdraw(50)
+    subject.withdraw(account,   50)
     expect(subject.funds).to eq 950
+  end
+
+  it 'allow withdraw if account has funds' do
+    expected_output = { status: true,
+                        message: 'success',
+                        date: Date.today,
+                        amount: 45 }
+
+    expect(subject.withdraw(account, 45)).to eq expected_output
   end
 end
